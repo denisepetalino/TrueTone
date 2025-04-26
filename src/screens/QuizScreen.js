79dd8d86e2d0a2e63be1e36b1dfa920f4485c3fc
denisepetalino <denisepetalino@gmail.com>
2025-04-26 19:01:17 +0100

    quiz screen navbar

--- src/screens/QuizScreen.js
+++ src/screens/QuizScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import Navbar from '../components/Navbar';
 
 function QuizScreen(props) {
     return (
         <View style={styles.container}>
             <Text style={styles.text}> Quiz Screen </Text>
+        <Navbar/>
         </View>
     );
 }
